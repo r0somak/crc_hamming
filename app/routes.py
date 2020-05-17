2245a61e109267b61da7bd7fbeb669c7538c5025
r0somak <mikolajakartel@gmail.com>
2020-05-17 17:10:39 +0200

    Colorization of corrupted bits

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template
 from flask import jsonify
-from flask import redirect
 from flask import request
+from flask import make_response
 
 from app import app
 from app.form import CrcForm
@@ -12,10 +12,13 @@ from app.utils import generate_crc, zfill_crc
 def create_form():
     crc_form = CrcForm()
     print(f"FORM ERRORS: {crc_form.errors}")
-    return render_template('base.html', form=crc_form)
+    return make_response(render_template('base.html', form=crc_form), 200)
 
 @app.route('/hamming', methods=['POST'])
 def generate_hamming():
+    input_data = request.values['inputData']
+    if len(input_data) < 10:
+        return make_response(f"Input data has {len(input_data)} chars, minimum 10 required", 400)
     crc = generate_crc(request.values['inputData'], request.values['crcType'])
     crc = zfill_crc(crc)
 
@@ -25,9 +28,35 @@ def generate_hamming():
 
     inp_crc = binput + crc
 
-    return jsonify({
+    return make_response(jsonify({
         "crc": crc,
         "bitInputData": binput_8,
         "inp_crc": inp_crc,
         "hamming": hamming.encode(inp_crc),
-    })
+    }), 200)
+
+@app.route('/corrupt', methods=['POST'])
+def corrupt_data():
+    data = request.values['inputData']
+    lst_data = list(data)
+    positions = request.values['positions'].split(',')
+    try:
+        positions = [int(i.strip()) for i in positions]
+    except ValueError as e:
+        return make_response("Invalid input data", 400)
+    positions.sort()
+    if positions[-1] > len(data):
+        return make_response(u"Last position exceeds input data length.", 400)
+    lst_pos = list(positions)
+    for i in range(len(lst_pos)):
+        if lst_data[lst_pos[i]] == 1:
+            lst_data[lst_pos[i]] = 0
+        else:
+            lst_data[lst_pos[i]] = 1
+    lst_data = [str(i) for i in lst_data]
+    new_data = ''.join(lst_data)
+    print(positions)
+    return make_response(jsonify({
+        "corruptedData": new_data,
+        "corruptedPositions": positions,
+    }), 200)
--- app/static/js/test.js
+++ app/static/js/test.js
@@ -4,19 +4,24 @@ $( document ).ajaxStart(function() {
     $( "#loading" ).hide();
   });
 
-function colorizeHamming(str, id){
-        let coloredHamming = "";
-        let potega = 1;
-        for (let i = 0; i<str.length; i++) {
-            if (i === potega - 1) {
-                potega += potega;
-                coloredHamming += str.charAt(i).fontcolor("red");
-            }else {
-                coloredHamming += str[i];
-            }
+function colorizeHamming(str, id, positions=null){
+    let array = str.split('')
+    let potega = 1;
+    for (let i = 0; i<str.length; i++) {
+        if (i === potega - 1) {
+            potega += potega;
+            array[i] = str.charAt(i).fontcolor('green');
+        }else {
+            array[i] = str[i];
         }
-        let s1 = document.getElementById(id);
-        s1.innerHTML = coloredHamming;
+    }
+    if (positions != null){
+        for(let i = 0; i < positions.length; i++){
+            array[positions[i]] = str.charAt(positions[i]).fontcolor('red');
+        }
+    }
+    let s1 = document.getElementById(id);
+    s1.innerHTML = array.join('');
 }
 
 function hamming(){
@@ -30,13 +35,34 @@ function hamming(){
         dataType : "json"
     })
     .done(function(response){
-        $("#datatable").show();
         $("#0").text(response['bitInputData']);
         $("#1").text(response['crc']);
         $("#2").text(response['inp_crc']);
-        colorizeHamming(response['hamming'], 3)
+        colorizeHamming(response['hamming'], 3);
+        $("#datatable").show();
+    }).fail(function(response){
+        $("#0").html(response.responseText.fontcolor('red'));
+        $("#datatable").show();
+    });
+}
 
-    }).fail(function(){
-        $("#0").text("CHINA BAD");
+function corrupt(){
+    let hamm = document.getElementById(3);
+    console.log(hamm.innerText)
+    $.ajax({
+        url : "http://localhost:5000/corrupt",
+        method : 'post',
+        data : {
+            inputData : hamm.innerText,
+            positions : $("#corBits").val()
+        },
+        dataType : "json"
+    })
+    .done(function(response){
+        colorizeHamming(response['corruptedData'], 4, response['corruptedPositions']);
+        $( "#corTable" ).show();
+    }).fail(function(response){
+        $("#4").html(response.responseText.fontcolor('red'));
+        $( "#corTable" ).show();
     });
 }
